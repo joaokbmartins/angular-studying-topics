9ef65df0cd648ab6a7bb71b004b391eac82580d0
João Martins <martins.joao1909@gmail.com>
2020-11-26 07:53:25 -0300

    Restructuring project to cover other 'while-studying' topics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { BasicHighLightDirective } from './shared/directives/basic-highlight.directive';
-import { BetterHighLightDirective } from './shared/directives/better-highlight.directive';
-import { UnlessDirective } from './shared/directives/unless.directive';
+import { AppComponent } from './app.component';  
+import { BasicHighLightDirective } from './topic-directives/shared/directives/basic-highlight.directive';
+import { BetterHighLightDirective } from './topic-directives/shared/directives/better-highlight.directive';
+import { UnlessDirective } from './topic-directives/shared/directives/unless.directive';
+import { GeneralComponent } from './topic-directives/components/general/general.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BasicHighLightDirective,
     BetterHighLightDirective,
-    UnlessDirective
+    UnlessDirective,
+    GeneralComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/topic-directives/components/general/general.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-general',
+  templateUrl: "./general.component.html"
+})
+export class GeneralComponent {
+  
+  title = 'Directives';
+
+  value = 10;
+
+}
